19c03c07465b5aeb50d340683c9179f00145644f
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-11-04 16:14:38 -0300

    REST successfully tested

---
+++ src/main/java/com/loginscreen/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.loginscreen.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public String testRest(){
+        return "REST configured correctly";
+    }
+}
