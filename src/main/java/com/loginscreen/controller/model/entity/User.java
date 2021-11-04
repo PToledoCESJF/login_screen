ec22df563028b5eee3d06cd152d535032cbaafea
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-11-04 18:45:39 -0300

    Creating the User endity JPA mapping

--- src/main/java/com/loginscreen/controller/model/entity/User.java
+++ src/main/java/com/loginscreen/controller/model/entity/User.java
@@ -6,6 +6,10 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @NoArgsConstructor
@@ -13,10 +17,14 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity(name = "tb_users")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
 
