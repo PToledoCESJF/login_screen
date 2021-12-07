e758dc647231bd93b5b7729d092465e8a2424464
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-12-07 19:47:22 -0300

    Creating utils.ts from AuthProvider

---
+++ frontend/src/context/AuthProvider/utils.ts
@@ -0,0 +1,22 @@
+import { Api } from "../../service/api";
+import { iUser } from "./types";
+
+export function setUserLocalStorage(user: iUser | null){
+  localStorage.setItem("US", JSON.stringify(user));
+}
+
+export function getUserLocalStorage(){
+  const json = localStorage.getItem("US");
+  if(!json) return null;
+  const user = JSON.parse(json)
+  return user ?? null;
+}
+
+export async function LoginRequest(email: string, password: string) {
+  try {
+    const request = await Api.post("login", { email, password });
+    return request.data;
+  } catch (error) {
+    return null;    
+  }
+}
