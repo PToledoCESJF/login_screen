b3932fd7fc776c1538a70abc53a9db2af5ec050d
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-12-07 20:00:30 -0300

    Creating the AuthProvider Functional Component and its authenticate and logout functions in index.tsx

--- frontend/src/context/AuthProvider/index.tsx
+++ frontend/src/context/AuthProvider/index.tsx
@@ -1,4 +1,34 @@
-import { createContext } from "react";
-import { iContext } from "./types";
+import { createContext, useEffect, useState } from "react";
+import { iAuthProvider, iContext, iUser } from "./types";
+import { getUserLocalStorage, LoginRequest, setUserLocalStorage } from "./utils";
 
 export const AuthContext = createContext<iContext>({} as iContext)
+
+export const AuthProvider = ({ children }: iAuthProvider) => {
+  const [user, setUser] = useState<iUser | null>();
+
+  useEffect(() => {
+    const user = getUserLocalStorage();
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
+  async function authenticate(email: string, password: string) {
+    const response = await LoginRequest(email, password);
+    const payload = { token: response.token, email };
+    setUser(payload);
+    setUserLocalStorage(payload);
+  }
+
+  function logout() {
+    setUser(null);
+    setUserLocalStorage(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ ...user, authenticate, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
