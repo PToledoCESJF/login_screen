3f751ab389b78faa27920c590991301cadba5121
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-12-12 17:15:03 -0300

    Searching for localStorage username and token

--- frontend/src/service/api.ts
+++ frontend/src/service/api.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
+import { getUserLocalStorage } from "../context/AuthProvider/utils";
 
 export const Api = axios.create({
   baseURL: 'http://localhost:8080/'
 });
+
+Api.interceptors.request.use(
+  (config: any) => {
+    const user = getUserLocalStorage();
+    config.headers.Authorization = user?.token;
+    return config;
+  },
+  (error: any) => {
+    return Promise.reject(error)
+  }
+)
